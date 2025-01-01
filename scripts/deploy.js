d4c3f2a9a3a01ea2476c80254b55d07969a5fdcc
mujeebSulayman <sulaymanmujeeb6@gmail.com>
2025-01-01 13:04:43 +0100

    deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,4 +2,33 @@ async function main() {
 	const [deployer] = await ethers.getSigners();
 
 	console.log('Deploying contracts with the account:', deployer.address);
+
+	const balance = await deployer.getBalance();
+	console.log('Account balance:', balance.toString());
+
+	const HemReward = await ethers.getContractFactory('HemReward');
+
+	const initialSupply = ethers.utils.parseEther('1000000');
+	console.log('Initial supply:', initialSupply.toString());
+
+	const maxSupply = ethers.utils.parseEther('10000000');
+	console.log('Max supply:', maxSupply.toString());
+
+	const referrerReward = ethers.utils.parseEther('50', 'wei');
+	console.log('Referrer reward:', referrerReward.toString());
+
+	const hemReward = await HemReward.deploy(
+		initialSupply,
+		maxSupply,
+		referrerReward
+	);
+	await hemReward.deployed();
+
+	console.log('HemReward address:', hemReward.address);
 }
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
