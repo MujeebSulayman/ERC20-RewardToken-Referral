81dcb8ca8630816567d839445aeb183a1d5dd61c
mujeebSulayman <sulaymanmujeeb6@gmail.com>
2025-01-01 11:02:41 +0100

    mint reward

--- contracts/HemReward.sol
+++ contracts/HemReward.sol
@@ -5,13 +5,25 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HemReward is ERC20, Ownable {
-    mapping (address => address) referrals;
-    mapping (address => uint256) referralRewards;
+    mapping(address => address) referrals;
+    mapping(address => uint256) referralRewards;
 
     uint256 public maxSupply;
     uint256 public totalMinted;
 
-    
+    event referralRewardsClaimed(address indexed referrer, uint256 reward);
+    event rewardDistributed(address indexed user, uint256 amount);
 
-}
+    constructor(
+        uint256 _initialSupply,
+        uint256 _maxSupply
+    ) ERC20("HemReward", "HMR") Ownable(msg.sender) {
+        _mint(msg.sender, _initialSupply * (10 ** decimals()));
+        maxSupply = _maxSupply * (10 ** decimals());
+        totalMinted = _initialSupply * (10 ** decimals());
+    }
+
+   function mint( address to, uint256 amount ) external onlyOwner {
     
+   }
+}
