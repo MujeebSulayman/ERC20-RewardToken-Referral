6cbc95de4a07b538fa20e667a56ad77314d85f24
mujeebSulayman <sulaymanmujeeb6@gmail.com>
2025-01-10 05:23:09 +0100

    dep

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-   extends: ['next', 'next/core-web-vitals'],
-   env: {
-     node: true,
-     browser: true,
-   },
-   rules: {
-     'import/no-anonymous-default-export': 'off',
-   },
- }
- 
+  extends: ['next', 'next/core-web-vitals'],
+  env: {
+    node: true,
+    browser: true,
+  },
+  rules: {
+    'import/no-anonymous-default-export': 'off',
+  },
+  // Removed deprecated options
+  // useEslintrc: false,
+  // extensions: ['.js', '.jsx', '.ts', '.tsx'],
+}
