ef42ca1967138ccb516eb660f815043b442110d8
mujeebSulayman <sulaymanmujeeb6@gmail.com>
2025-01-10 05:26:50 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,24 @@
 module.exports = {
-  extends: ['next', 'next/core-web-vitals'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   env: {
     node: true,
     browser: true,
+    es2021: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
     'import/no-anonymous-default-export': 'off',
-  },
-  // Removed deprecated options
-  // useEslintrc: false,
-  // extensions: ['.js', '.jsx', '.ts', '.tsx'],
-}
+    '@typescript-eslint/no-unused-vars': 'warn'
+  }
+};
