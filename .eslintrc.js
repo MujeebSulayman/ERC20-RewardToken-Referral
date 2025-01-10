55af156bc536decb3a3083d4c85e2dd0a22fd543
mujeebSulayman <sulaymanmujeeb6@gmail.com>
2025-01-10 05:13:43 +0100

    Optimized

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+   extends: ['next', 'next/core-web-vitals'],
+   env: {
+     node: true,
+     browser: true,
+   },
+   rules: {
+     'import/no-anonymous-default-export': 'off',
+   },
+ }
+ 
