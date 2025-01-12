577dea6aeb17e1548c71e370dc63e94679898495
mujeebSulayman <sulaymanmujeeb6@gmail.com>
2025-01-12 09:44:38 +0100

    git update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    
+    config.resolve.alias['@artifacts'] = path.resolve(__dirname, 'artifacts');
     
     config.module.rules.push({
       test: /\.(sol|json)$/,
